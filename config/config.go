c62758c3035c10094d98a0f03b66425bf90bd356
Yusuf Yaman <nxjoseph@protonmail.com>
2024-08-17 16:52:09 +0300

    change config for freebsd rc.d script

--- config/config.go
+++ config/config.go
@@ -6,12 +6,9 @@ import (
 )
 
 func Path(filename string) (string, error) {
-	configHome, err := os.UserConfigDir()
-	if err != nil {
-		return "", err
-	}
+	configHome := "/var/db/hydroxide"
 
-	p := filepath.Join(configHome, "hydroxide", filename)
+	p := filepath.Join(configHome, filename)
 
 	dirname, _ := filepath.Split(p)
 	if err := os.MkdirAll(dirname, 0700); err != nil {
